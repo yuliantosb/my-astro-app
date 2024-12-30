ccb9d229ccc0bc831580adf7a3254059a26df3af
yuliantosb <yulianto.saparudin@gmail.com>
2024-12-30 14:56:30 +0700

    finish tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://my-astro-app-yulianto.netlify.app",
+  integrations: [preact()]
+});
